b64f03ebe9ff9705c8bdb8356a20caa6f04f6790
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-20 21:54:31 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -40,7 +40,7 @@ bot_token = os.environ.get("BOT_TOKEN")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
 sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
 
-app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token, workers=32)
+app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token, workers=2)
 
 data = []
 
